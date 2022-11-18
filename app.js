8e228b198960d1b689350af0b031b15745d2f186
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2022-11-18 13:26:32 +0630

    Updated pofolio

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.navbar_menu');
+const navMenu = document.querySelector('#navbar');
+const helloBtn = document.querySelector('#Hello');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  helloBtn.classList.toggle('active');
 });
 
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+document.querySelectorAll('.nav__link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
+  helloBtn.classList.remove('active');
 }));
